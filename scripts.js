ee9dabe67de05b5937f7a22409175488ea9573e3
g33zy <stevejgaz@gmail.com>
2022-11-16 22:25:36 -0600

    check for win

--- scripts.js
+++ scripts.js
@@ -181,7 +181,7 @@ const horizontalWin = () => {
 const verticalWin = () => {
   // Your code here to check for vertical wins
   if((board[0][0] == "X" && board[1][0] == "X" && board[2][0] == "X") 
-         || (board[0][0] == "O" && board[0][1] == "O" && board[0][2] == "O")){
+         || (board[0][0] == "O" && board[1][0] == "O" && board[2][0] == "O")){
           return true
          }
   
@@ -203,8 +203,8 @@ const diagonalWin = () => {
           return true
         }
 
-  if((board[0][2] == "X" && board[1][1] == "X" && board[0][2] == "X") 
-        || (board[0][2] == "O" && board[1][1] == "O" && board[0][2] == "O")){
+  if((board[0][2] == "X" && board[1][1] == "X" && board[2][0] == "X") 
+        || (board[0][2] == "O" && board[1][1] == "O" && board[2][0] == "O")){
           return true
         }
 }
